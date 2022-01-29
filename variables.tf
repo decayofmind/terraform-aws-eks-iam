18144e0d3ae364107f0ede1cf411449d539dd947
Roman Komkov <r.komkov@gmail.com>
2022-01-29 15:26:41 +0100

    Add some missing variables

--- variables.tf
+++ variables.tf
@@ -5,3 +5,11 @@ variable "cluster_oidc_issuer_url" {
 variable "cluster_id" {
   type = string
 }
+
+variable "service_account_name" {
+  type = string
+}
+
+variable "service_account_namemspace" {
+  type = string
+}
